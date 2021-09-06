b2e86060dc10b0d21fae6fd955f995b70e123763
Jobayer Habib Afsan <85637295+AfsanHabib@users.noreply.github.com>
2021-09-06 22:27:13 +0600

    Add files via upload

---
+++ Codeforces-Python/122A - Lucky Division.py
@@ -0,0 +1,12 @@
+# Solved
+
+number=int(input())
+
+if number%4==0 or number%7==0 or number%47==0 or number%74==0 or number%77==0 or number%447==0 or number%474==0 or number%477==0 or number%747==0 or number%774==0 or number%777==0:
+    print('YES')
+else:
+    print('NO')
+
+
+
+#  4, 7, 47, 74, 77, 447, 474, 477, 747, 774, 777
