338ace97ff4e2a3a966e4c32f0d84161e7d60ece
Afsan Habib <85637295+AfsanHabib@users.noreply.github.com>
2021-12-14 23:06:18 +0600

    Add files via upload

---
+++ Codeforces-C++/contest-1618-problem-A Polycarp and Sums of Subsequences.cpp
@@ -0,0 +1,12 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main(){
+int t,a1,a2,a3,a4,a5,a6,a7;
+cin>>t;
+while(t--){
+    cin>>a1>>a2>>a3>>a4>>a5>>a6>>a7;
+    cout<<(a7-a6)<<" "<<a2<<" "<<(a6-a2)<<endl;
+}
+
+return 0;
+}
